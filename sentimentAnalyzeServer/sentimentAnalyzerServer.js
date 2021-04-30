3af30ee923afc8acf920cf4e6a4695668f7ee24b
Yahya Zini <yahyazini@gmail.com>
2021-04-29 22:41:48 -0400

    generated code and dependencies

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,33 +1,32 @@
-const express = require('express');
+const express = require("express");
+
 const app = new express();
 
-app.use(express.static('client'))
+app.use(express.static("client"));
 
-const cors_app = require('cors');
+const cors_app = require("cors");
 app.use(cors_app());
 
-app.get("/",(req,res)=>{
-    res.render('index.html');
-  });
-
-app.get("/url/emotion", (req,res) => {
-
-    return res.send({"happy":"90","sad":"10"});
+app.get("/", (req, res) => {
+  res.render("index.html");
 });
 
-app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+app.get("/url/emotion", (req, res) => {
+  return res.send({ happy: "90", sad: "10" });
 });
 
-app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+app.get("/url/sentiment", (req, res) => {
+  return res.send("url sentiment for " + req.query.url);
 });
 
-app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+app.get("/text/emotion", (req, res) => {
+  return res.send({ happy: "10", sad: "90" });
+});
+
+app.get("/text/sentiment", (req, res) => {
+  return res.send("text sentiment for " + req.query.text);
 });
 
 let server = app.listen(8080, () => {
-    console.log('Listening', server.address().port)
-})
-
+  console.log("Listening", server.address().port);
+});
